aee9ba1062d23f8e493e2678f67e596dd325b8a7
dskripka <skripka.dmitry@gmail.com>
2019-05-21 21:35:07 +0300

    validation form added

--- src/cw19/views.py
+++ src/cw19/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cw19.forms import PostForm
 
 # Create your views here.
 
@@ -7,10 +8,16 @@ def comment_add(request):
     if request.method == 'GET':
         return render(request, 'comment_add.html')
     elif request.method == 'POST':
-        firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
-        age = request.POST.get('age')
-        comment = request.POST.get('comment')
-        print(f'{firstname}|{lastname}|{age}|{comment}')
-        return render(request, 'comment_add.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            data = form.data
+            firstname = data.get('firstname')
+            lastname = data.get('lastname')
+            age = data.get('age')
+            comment = data.get('comment')
+            print(f'{firstname}|{lastname}|{age}|{comment}')
+            return render(request, 'comment_add.html')
+        else:
+            errors = form.errors
+            return HttpResponse(f'{errors}')
     return HttpResponse('Wrong request method')
