91f3e8a8a39e93a357924e2e25365a271c5d1108
dskripka <skripka.dmitry@gmail.com>
2019-05-21 21:52:55 +0300

    use django form for template

--- src/cw19/views.py
+++ src/cw19/views.py
@@ -6,17 +6,19 @@ from cw19.forms import PostForm
 
 def comment_add(request):
     if request.method == 'GET':
-        return render(request, 'comment_add.html')
+        context = {'form': PostForm()}
+        return render(request, 'comment_add.html', context)
     elif request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            data = form.data
+            data = form.cleaned_data
             firstname = data.get('firstname')
             lastname = data.get('lastname')
             age = data.get('age')
             comment = data.get('comment')
             print(f'{firstname}|{lastname}|{age}|{comment}')
-            return render(request, 'comment_add.html')
+            context = {'form': PostForm()}
+            return render(request, 'comment_add.html', context)
         else:
             errors = form.errors
             return HttpResponse(f'{errors}')
