9850fcb0d7cac076434040d34b7f0afd57f70717
dskripka <skripka.dmitry@gmail.com>
2019-05-21 20:11:31 +0300

    cw19 first commit

---
+++ src/cw19/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def comment_add(request):
+    if request.method == 'GET':
+        return render(request, 'comment_add.html')
+    elif request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        age = request.POST.get('age')
+        comment = request.POST.get('comment')
+        print(f'{firstname}|{lastname}|{age}|{comment}')
+        return render(request, 'comment_add.html')
+    return HttpResponse('Wrong request method')
--- src/src/settings.py
+++ src/src/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
+    'cw19',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
